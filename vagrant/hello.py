9e976d94ce59434b70dccd35bc3a6418c76f12a1
Johnny <johnnychan0302@gmail.com>
2015-08-30 17:37:36 +0100

    add routes for index and hello pages

--- vagrant/hello.py
+++ vagrant/hello.py
@@ -5,8 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello Page'
 
 if __name__ == '__main__':
 	app.debug = True
