5c4dcb3880730f510c7510d62c57dd4e5fee7697
Johnny <johnnychan0302@gmail.com>
2015-08-30 17:47:32 +0100

    add dynamic routes for username and post_id

--- vagrant/hello.py
+++ vagrant/hello.py
@@ -13,6 +13,16 @@ def index():
 def hello():
     return 'Hello Page'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'username: %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'post_id: %d' % post_id
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0')
